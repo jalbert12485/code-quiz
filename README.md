e410c9a97f861cbcfee84d43e3b0862267b07b64
Justin Albert <jalbert@carthage.edu>
2020-09-29 00:17:25 -0500

    started homework

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# homework-4
+Homework 4
+
+This work is done as an assignment for coding bootcamp.  The file provides a coding quiz for the taker in such a way that 
+i) The quiz will be timed.
+ii) One multiple choice question will appear on the screen at a time.
+iii) When a question is answer, the next question will appear.
+iv) If a question is incorrect, a portion of the time left will be taken away.
+v) When the time runs out, or all questions are answered, the quiz will end.
+vi) The score for the quiz will be the amount of time left on completion.
+vii) This score will then be saved so that highscores can be compared.
---
+++ script.js
@@ -0,0 +1,74 @@
+var question0={
+    question: "Question 0",
+    answer1: "Q0 Answer 1",
+    answer2: "Q0 Answer 2",
+    answer3: "Q0 Answer 3",
+    answer4: "Q0 Answer 4"
+}
+var question1={
+    question: "Question 1",
+    answer1: "Q1 Answer 1",
+    answer2: "Q1 Answer 2",
+    answer3: "Q1 Answer 3",
+    answer4: "Q1 Answer 4"
+}
+var question2={
+    question: "Question 2",
+    answer1: "Q2 Answer 1",
+    answer2: "Q2 Answer 2",
+    answer3: "Q2 Answer 3",
+    answer4: "Q2 Answer 4"
+}
+var question3={
+    question: "Question 3",
+    answer1: "Q3 Answer 1",
+    answer2: "Q3 Answer 2",
+    answer3: "Q3 Answer 3",
+    answer4: "Q3 Answer 4"
+}
+
+
+var questions=[question0, question1, question2, question3];
+
+var questionBox=document.querySelector("#question");
+var answerBox1=document.querySelector("#answer1Label");
+var answerBox2=document.querySelector("#answer2Label");
+var answerBox3=document.querySelector("#answer3Label");
+var answerBox4=document.querySelector("#answer4Label");
+var submitBtn=document.querySelector("#submit-btn");
+var currentQuestion=0;
+var timeLeft=5;
+var timeBox=document.querySelector("#time");
+
+
+function changeQuestion() {
+    if(currentQuestion===0){
+        timeRemaining();
+    }
+    if(currentQuestion< questions.length){
+    questionBox.textContent=questions[currentQuestion].question;
+    answerBox1.textContent=questions[currentQuestion].answer1;
+    answerBox2.textContent=questions[currentQuestion].answer2;
+    answerBox3.textContent=questions[currentQuestion].answer3;
+    answerBox4.textContent=questions[currentQuestion].answer4;
+    }
+    currentQuestion++;
+
+}
+
+submitBtn.addEventListener("click",changeQuestion);
+
+
+function timeRemaining(){
+    var timerInterval=setInterval(function(){
+        timeLeft--;
+        timeBox.textContent="Time left: "+ timeLeft;
+        if(timeLeft==0 || (currentQuestion > questions.length)){
+            clearInterval(timerInterval);
+        }
+    },1000)
+
+    
+}
+
+
