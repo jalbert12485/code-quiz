19f88de0f864897c5cbc981317dddfdd24921d14
Justin Albert <jalbert@carthage.edu>
2020-09-30 16:10:18 -0500

    dried out code

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 var question0={
-    question: "Question 0",
-    answer1: "Q0 Answer 1",
-    answer2: "Q0 Answer 2",
-    answer3: "Q0 Answer 3",
-    answer4: "Q0 Answer 4",
-    correctAnswer: 1
+    question: "In which HTML tag does the javascript go?",
+    answer: ["<scripting>",
+    "<script>",
+    "<js>",
+     "<javascript>"],
+    correctAnswer: 2
 }
 var question1={
-    question: "Question 1",
-    answer1: "Q1 Answer 1",
-    answer2: "Q1 Answer 2",
-    answer3: "Q1 Answer 3",
-    answer4: "Q1 Answer 4",
-    correctAnswer: 2
+    question: "Which of the following will change the tag to say 'Hello World' <p id='demo'>This is a demonstration</p>",
+    answer: ["document.querySelector('demo').textContent='Hello World!'",
+    "document.GetElementById('demo').text='Hello World!'",
+    "document.querySelector('#demo').innerHTML='Hello World!'",
+    "document.querySelector('.demo').textContent='Hello World!'"],
+    correctAnswer: 3
 }
 var question2={
     question: "Question 2",
-    answer1: "Q2 Answer 1",
-    answer2: "Q2 Answer 2",
-    answer3: "Q2 Answer 3",
-    answer4: "Q2 Answer 4",
+    answer: ["Q2 Answer 1",
+    "Q2 Answer 2",
+    "Q2 Answer 3",
+     "Q2 Answer 4"],
     correctAnswer: 1
 }
 var question3={
     question: "Question 3",
-    answer1: "Q3 Answer 1",
-    answer2: "Q3 Answer 2",
-    answer3: "Q3 Answer 3",
-    answer4: "Q3 Answer 4",
+    answer: ["Q3 Answer 1",
+    "Q3 Answer 2",
+    "Q3 Answer 3",
+     "Q3 Answer 4"],
     correctAnswer: 4
 }
 
 var highScore = localStorage.getItem("highScore");
+
+if(highScore===null){
+        localStorage.setItem("highScore", 0);
+        highScore=0;
+
+    }
+
 var highScoreBox=document.querySelector("#viewHighScore");
 highScoreBox.textContent="High Score: " + highScore;
 
@@ -42,27 +49,38 @@ var answerBox1=document.querySelector("#answer1Label");
 var answerBox2=document.querySelector("#answer2Label");
 var answerBox3=document.querySelector("#answer3Label");
 var answerBox4=document.querySelector("#answer4Label");
+var answerBox=[answerBox1,answerBox2,answerBox3,answerBox4];
 var submitBtn=document.querySelector("#submit-btn");
 var currentQuestion=0;
 var timeLeft=60;
 var timeBox=document.querySelector("#time");
+var fieldSet = document.querySelector("fieldset");
+var answerCont=document.querySelector("#answer-cont");
 
 
 function changeQuestion() {
     if(currentQuestion===0){
         timeRemaining();
+        submitBtn.textContent="Submit Answer";
+        answerCont.setAttribute("style", "visibility: visible");
+
     }
     if(0 < currentQuestion && currentQuestion<= questions.length){
         checkAnswer();
     }
     if(currentQuestion< questions.length){
     questionBox.textContent=questions[currentQuestion].question;
-    answerBox1.textContent=questions[currentQuestion].answer1;
-    answerBox2.textContent=questions[currentQuestion].answer2;
-    answerBox3.textContent=questions[currentQuestion].answer3;
-    answerBox4.textContent=questions[currentQuestion].answer4;
+        for(var i=0; i< 4; i++){
+    answerBox[i].textContent=questions[currentQuestion].answer[i];
+            }
     }
     currentQuestion++;
+    submitBtn.disabled=true;
+    if(currentQuestion < questions.length+1){
+    setTimeout(function(){
+        submitBtn.disabled=false;
+    },1000);}
+
 
 
 }
@@ -77,16 +95,13 @@ function timeRemaining(){
         if(timeLeft==0 || (currentQuestion > questions.length)){
             clearInterval(timerInterval);
             checkHighScore();
+            submitBtn.disabled=true;
         }
     },1000)
 
     
 }
 
-var answerCheck1=document.querySelector("#answer1");
-var answerCheck2=document.querySelector("#answer2");
-var answerCheck3=document.querySelector("#answer3");
-var answerCheck4=document.querySelector("#answer4");
 
 var userAnswer=1;
 
@@ -94,18 +109,24 @@ function changeUserAnswer(j){
     userAnswer=j;
 }
 
-answerCheck1.addEventListener("click",function (){
-    changeUserAnswer(1);
+
+
+
+
+fieldSet.addEventListener("click", function(event) {
+    var element = event.target;
+    if (element.matches("input")){
+        userAnswer=Number(element.getAttribute("data-answer"));
+    }
+    // if (element.matches("button"){
+    //     changeQuestion();
+    // })
 });
-answerCheck2.addEventListener("click",function (){
-    changeUserAnswer(2);
-});
-answerCheck3.addEventListener("click",function (){
-    changeUserAnswer(3);
-});
-answerCheck4.addEventListener("click",function (){
-    changeUserAnswer(4);
-});
+
+
+
+
+
 
 function checkAnswer() {
     if(userAnswer != questions[currentQuestion-1].correctAnswer){
@@ -116,9 +137,10 @@ function checkAnswer() {
 
 
 function checkHighScore(){
-    if(highScore===null || highScore < timeLeft){
+    if(highScore < timeLeft){
         localStorage.setItem("highScore", timeLeft);
         highScoreBox.textContent="High Score: " + timeLeft;
     }
 
 }
+
