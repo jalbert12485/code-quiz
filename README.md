d830c0ff06041aaecd85bdd849b405ef66c72fcf
Justin Albert <jalbert@carthage.edu>
2020-09-30 17:19:08 -0500

    added in questions

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ Homework 4
 This work is done as an assignment for coding bootcamp.  The file provides a coding quiz for the taker in such a way that 
 i) The quiz will be timed.
 ii) One multiple choice question will appear on the screen at a time.
-iii) When a question is answer, the next question will appear.
-iv) If a question is incorrect, a portion of the time left will be taken away.
+iii) When a question is answered, the next question will appear.
+iv) If a question is incorrect, a portion of the time left (10 seconds) will be taken away.
 v) When the time runs out, or all questions are answered, the quiz will end.
 vi) The score for the quiz will be the amount of time left on completion.
-vii) This score will then be saved so that highscores can be compared.
+vii) This score will then be compared to the highscore which is saved locally.
+
+Note that the questions are modified from those on the JavaScript quiz from W3Shool at https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Questions are modified from W3School's JavaScript quiz at https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
+
 var question0={
     question: "In which HTML tag does the javascript go?",
     answer: ["<scripting>",
@@ -15,21 +17,70 @@ var question1={
     correctAnswer: 3
 }
 var question2={
-    question: "Question 2",
-    answer: ["Q2 Answer 1",
-    "Q2 Answer 2",
-    "Q2 Answer 3",
-     "Q2 Answer 4"],
-    correctAnswer: 1
+    question: "Which of the following will correctly link an external script called ''script.js''",
+    answer: ["<script name=''script.js''></script>",
+    "<script href=''script.js''></script>",
+    "<script src=''script.js''></script>",
+     "<link href=''script.js''></link>"],
+    correctAnswer: 3
 }
 var question3={
-    question: "Question 3",
-    answer: ["Q3 Answer 1",
-    "Q3 Answer 2",
-    "Q3 Answer 3",
-     "Q3 Answer 4"],
+    question: "How would you write ''Hello World!'' in an alert box?",
+    answer: ["alert(''Hello World!'');",
+        "msg(''Hello World!'');",
+    "alertBox(''Hello World!'');",
+     "msgBox(''Hello Wolrd!'');"],
+    correctAnswer: 1
+}
+var question4={
+    question: "How do you create a function called ''myFunction'' in JavaScript?",
+    answer: ["function=myFunction()",
+        "function=myFunction()",
+    "function() myFunction",
+     "function myFunction()"],
     correctAnswer: 4
 }
+var question5={
+    question: "How would you call a function called ''myFunction'' in JavaScript?",
+    answer: ["call myFunction()",
+        "myFunction()",
+    "call function myFunction()",
+     "myFunction"],
+    correctAnswer: 2
+}
+var question6={
+    question: "Which is a valid if statement in JavaScript",
+    answer: ["if i==5 then {}",
+        "if(i==5) then {}",
+    "if i==5 {}",
+     "if(i==5) {}"],
+    correctAnswer: 4
+}
+var question7={
+    question: "How does a for loop start?",
+    answer: ["for i=1 to 5",
+        "for (i=0; i <=5)",
+    "for(i <=5; i++)",
+     "for(i=0; i <=5; i++)"],
+    correctAnswer: 4
+}
+var question8={
+    question: "How do you add a comment in JavaScript?",
+    answer: ["<!-- This is a comment -->",
+        " /This is a comment",
+    "// This is a comment  ",
+     "< This is a comment >"],
+    correctAnswer: 3
+}
+var question9={
+    question: "Which of the following is a properly declared array?",
+    answer: ["array myArray=[0,1,2]",
+        "var myArray=[0,1,2]",
+    "array myArray={ entry1: 1, entry2: 2, entry3: 3 }",
+     "var myArray={ entry1: 1, entry2: 2, entry3: 3 }"],
+    correctAnswer: 2
+}
+
 
 var highScore = localStorage.getItem("highScore");
 
@@ -42,7 +93,7 @@ if(highScore===null){
 var highScoreBox=document.querySelector("#viewHighScore");
 highScoreBox.textContent="High Score: " + highScore;
 
-var questions=[question0, question1, question2, question3];
+var questions=[question0, question1, question2, question3, question4, question5, question6, question7, question8, question9];
 
 var questionBox=document.querySelector("#question");
 var answerBox1=document.querySelector("#answer1Label");
